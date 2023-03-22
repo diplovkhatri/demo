e8a32d2b0445ccfc130e7af3c0ef577319337a2a
diplovkhatri <diplovkhatri88@gmail.com>
2023-03-22 07:57:28 +0545

    demo

--- src/pages/Vote/Proposal.js
+++ src/pages/Vote/Proposal.js
@@ -8,17 +8,33 @@ const Proposal = () => {
           Proposals
         </div>
         {/*  */}
-        <div className=" flex">
-          <p className="opacity-60 w-[600px]  grid place-content-center m-3 text-xl">
+        <div className="flex">
+          <p className="opacity-60   grid place-content-center m-3 text-xl">
             Connect wallet to make a propsal
           </p>
-          <button className="border-4 m-3 w-1/3 bg-slate-900 text-white rounded-2xl text-xl font-semibold hover:bg-green-900 hover:border-green-300">
+          <button className="border-4 mt-3 mb-3 bg-slate-900 text-white rounded-2xl text-xl font-semibold hover:bg-green-900 hover:border-green-300">
             <p className="opacity-80 m-2">submit proposal</p>
           </button>
         </div>
         {/*  */}
       </div>
       {/* proposal */}
+      <div className="flex flex-col border-2 p-3 rounded-2xl border-zinc-500 m-2">
+        <div className="flex justify-between">
+          <div className="flex h-[5vh] ">
+            <span className="opacity-40 text-3xl font-semibold">245</span>
+            <span className="text-3xl font-semibold ml-4">proposal</span>
+          </div>
+          <div>
+            <button className="border-zinc-500 border-2 p-1 w-full rounded-xl">
+              pending
+            </button>
+          </div>
+        </div>
+        <div className="opacity-50 border-2 w-1/4 text-lg rounded-xl border-zinc-500 grid place-content-center">
+          starts in 2 days
+        </div>
+      </div>
 
       <div className="flex flex-col border-2 p-3 rounded-2xl border-zinc-500 m-2">
         <div className="flex justify-between">
@@ -36,6 +52,8 @@ const Proposal = () => {
           starts in 2 days
         </div>
       </div>
+
+      {/* end */}
     </div>
   );
 };
--- src/pages/Vote/Treasury.js
+++ src/pages/Vote/Treasury.js
@@ -7,21 +7,21 @@ const Treasury = () => {
 
       <div className="w-full flex rounded-xl p-2">
         {/*  */}
-        <div className="border-4 md:overflow-hidden shadow-xl p-2 m-2 rounded-xl border-black w-1/4">
+        <div className="border-2 hover:border-green-300 md:overflow-hidden shadow-xl p-2 m-2 rounded-xl border-black w-1/4">
           <span className="text-stone-600 font-semibold text-2xl opacity-80 ">
             Eth
           </span>
           <h3 className="mt-2 mb-2 font-bold text-4xl">283150</h3>
         </div>
         {/*  */}
-        <div className="border-4 shadow p-2 m-2 rounded-xl w-1/3 border-black">
+        <div className="border-2 hover:border-green-300 shadow p-2 m-2 rounded-xl w-1/3 border-black">
           <span className="text-stone-600 font-semibold text-2xl opacity-80 ">
             $ USD
           </span>
           <h3 className="mt-2 mb-2 font-bold text-4xl">28350</h3>
         </div>
         {/*  */}
-        <div className="border-4  shadow p-2 m-2 rounded-xl w-1/2 border-black">
+        <div className="border-2 hover:border-green-300 shadow p-2 m-2 rounded-xl w-1/2 border-black">
           This treasury exists for JUJUS DAO participants to allocate resources
           for the long-term growth and prosperity of the JUJUS project.
         </div>
